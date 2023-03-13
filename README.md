0a736c836f3ea330f1a922d46cf0fc2b4b619937
Fabio Simonetti <fabio.simonetti@indigoway.com>
2023-03-13 17:23:33 -0300

    Some updates

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # tools-and-scripts
+
+## Some of my scripts
+
+### clean_docker_for_mac.sh
+
+For delete images and containers
+
+### google_gmb_extract_insights.py
+
+Google My Business insights extraction
+
+### google_places_and_whois_scrapper.py
+
+Google places API extract information and load info from whois command
+
+### odoo-pg_dump_bash_example.sh
+
+Odoo pg-dump bash example
+
+### odoo-restore-backup.sh
+
+Odoo restore database files (db and filestore)
+
+### selenium_secretaria_educacao_sp.py
+
+Extract Schools from Secretaria da educação SP
--- odoo-restore-backup.sh
+++ odoo-restore-backup.sh
@@ -63,31 +63,29 @@ echo "odoo-restore-backup | Parameters NEW_DB_NAME: $NEW_DB_NAME"
 echo "odoo-restore-backup | Parameters ODOO_CONF_PATH: $ODOO_CONF_PATH"
 
 
-# Executa o comando PSQL para matar as conexões do banco
+# Execute PSQL command to kill connections
 PGPASSWORD="odoo" psql -U odoo -p 6432 -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = '$DB_NAME'"
 PGPASSWORD="odoo" psql -U odoo -p 6432 -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = '$NEW_DB_NAME'"
-# Executa o comando PSQL para apagar o banco
+# Execute PSQL command to DROP database 
 PGPASSWORD="odoo" psql -U odoo -p 6432 -c 'DROP DATABASE "'$DB_NAME'";'
-# Executa o comando PSQL para criar o banco
+# Execute PSQL command to create database
 PGPASSWORD="odoo" psql -U odoo -p 6432 -c 'CREATE DATABASE "'$DB_NAME'";'
-# Executa o comando PSQL para reestaelecer o dump
+# Execute PSQL command to drop database new name case exists
 PGPASSWORD="odoo" psql -U odoo -p 6432 -c 'DROP DATABASE "'$NEW_DB_NAME'";' 
-# Executa o comando PSQL para criar o banco
+# Execute PSQL command to create database
 PGPASSWORD="odoo" psql -U odoo -p 6432 -c 'CREATE DATABASE "'$NEW_DB_NAME'";'
-# Executa o comando PSQL para reestaelecer o dump
+# Execute PSQL command to restore dump file
 PGPASSWORD="odoo" psql -U odoo -p 6432 -d "$NEW_DB_NAME" < $DUMP_FILE_PATH
-# Executa o comando PSQL a limpeza do ir_cron
+# Execute PSQL command to clean ir_cron
 PGPASSWORD="odoo" psql -U odoo -p 6432 -d "$NEW_DB_NAME" -c "UPDATE public.ir_cron SET active = FALSE;"
-# Executa o comando PSQL a limpeza do ir_mail_server
+# Execute PSQL command to clean ir_mail_server
 PGPASSWORD="odoo" psql -U odoo -p 6432 -d "$NEW_DB_NAME" -c "UPDATE public.ir_mail_server SET active = FALSE;"
-# Executa o comando PSQL a limpeza do ir_mail_server
+# Execute PSQL command to cleanir_mail_server
 PGPASSWORD="odoo" psql -U odoo -p 6432 -d "$NEW_DB_NAME" -c 'UPDATE public.ir_config_parameter SET value = '"'"'http://localhost:8069'"'"' WHERE "key" = '"'"'web.base.url'"'"' ;'
-# Seta a senha padrão como admin ()
+# Set for all users default pasword (1234)
 PGPASSWORD="odoo" psql -U odoo -p 6432 -d "$NEW_DB_NAME" -c 'UPDATE public.res_users SET password = '"'"'$pbkdf2-sha512$25000$glBKydl7TwlBiPFeqzXGWA$bHNaubY8NZFR2hn6QkPk.bBHnkcCe2ceV3N56w9ObqThRpydpVFY.eptd/3TZunVKX9KwoYM3pSmAr58VVsxlg'"'"';'
-# Insert Token
-PGPASSWORD="odoo" psql -U odoo -p 6432 -d "$NEW_DB_NAME" -c 'INSERT INTO public.api_access_token ("token", user_id, expires, "scope", create_uid, create_date, write_uid, write_date) VALUES('"'"'access_token_4e4d28b653c2c366fbd33c56cba85bsf12312e6b'"'"', 2, '"'"'2055-04-06 15:20:40.000'"'"', '"'"'userinfo'"'"', 1, '"'"'2022-07-20 15:20:40.000'"'"', 1, '"'"'2022-07-20 15:20:40.000'"'"');'
 
-# Insert mail_hog
+# Insert mail_hog for sending mail
 PGPASSWORD="odoo" psql -U odoo -p 6432 -d "$NEW_DB_NAME" -c 'INSERT INTO public.ir_mail_server ("name", smtp_host, smtp_port, smtp_user, smtp_pass, smtp_encryption, smtp_debug, "sequence", active, create_uid, create_date, write_uid, write_date) VALUES('"'"'mailhog'"'"', '"'"'localhost'"'"', 1026, '"'"'contato@waybee.com.br'"'"', '"'"'email'"'"', '"'"'none'"'"', true, 1, true, 2, '"'"'2022-08-03'"'"', 2, '"'"'2022-08-03'"'"');'
 
 pwd
